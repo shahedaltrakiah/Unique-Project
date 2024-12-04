c58f990574cf3797b2ef3c8a626028fa43e3c73b
shahedaltrakiah <shahedaltrakiah@gmail.com>
2024-12-05 00:39:35 +0300

    UP

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-// import Head from './components/Head';
 import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
-      <Head 
-        title="Home - Unique"
-        description="Welcome to Unique, your go-to shop for stationery!"
-      />
       <Home/>
-
-
     </>
   );
 }
