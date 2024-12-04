efa0383e89669049b9d7cb29f10423ca00e61090
lwahdan <leenwahdan01@gmail.com>
2024-12-05 00:02:08 +0300

    update

--- frontend/src/routes/routes.jsx
+++ frontend/src/routes/routes.jsx
@@ -1,25 +1,25 @@
-import About from "../components/About";
+import About from "../components/pages/About";
 // import Breadcrumb from "../components/Breadcrumb";
-import Cart from "../components/Cart";
-import Checkout from "../components/Checkout";
-import Contact from "../components/Contact";
+import Cart from "../components/pages/Cart";
+import Checkout from "../components/pages/Checkout";
+import Contact from "../components/pages/Contact";
 // import Footer from "../components/Footer";
-import Head from "../components/Head";
+import Head from "../components/utils/Head";
 // import Hero from "../components/Hero";
-import Home from "../components/Home";
-import Login from "../components/Login";
+import Home from "../components/pages/Home";
+import Login from "../components/auth/Login";
 // import NavBar from "../components/NavBar";
-import ProductDetails from "../components/ProductDetails";
-import Profile from "../components/Profile";
-import ProfileInfo from "../components/ProfileInfo";
-import ProfileManage from "../components/ProfileManage";
-import ProfileOrder from "../components/ProfileOrder";
-import ProfileUpdate from "../components/ProfileUpdate";
-import Register from "../components/Register";
-import Sell from "../components/Sell";
-import Shop from "../components/Shop";
-import Wishlist from "../components/Wishlist";
-import Layout from "../components/Layout";
+import ProductDetails from "../components/pages/ProductDetails";
+import Profile from "../components/profile/Profile";
+import ProfileInfo from "../components/profile/ProfileInfo";
+import ProfileManage from "../components/profile/ProfileManage";
+import ProfileOrder from "../components/profile/ProfileOrder";
+import ProfileUpdate from "../components/profile/ProfileUpdate";
+import Register from "../components/auth/Register";
+import Sell from "../components/pages/Sell";
+import Shop from "../components/pages/Shop";
+import Wishlist from "../components/pages/Wishlist";
+import Layout from "../components/common/Layout";
 
 const routes = [
     {
