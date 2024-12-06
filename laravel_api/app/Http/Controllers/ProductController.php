55e66fbc0d5eb528210c42f56730d46692e118f6
lwahdan <leenwahdan01@gmail.com>
2024-12-06 21:01:29 +0300

    update

--- laravel_api/app/Http/Controllers/ProductController.php
+++ laravel_api/app/Http/Controllers/ProductController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Storage;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
