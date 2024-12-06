48bdd0907ed860259ab63c77f6d41a7b4a062057
issaSroor <issasroorofficial@gmail.com>
2024-12-06 23:40:44 +0300

    push

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -54,13 +54,15 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('favorites/{productId}', [FavoriteController::class, 'destroy']);
 
     // add product to favorites
-    Route::post('favorites', [FavoriteController::class, 'store']);
 
     // get all categories
     Route::get('categories', [CategoryController::class, 'index']);
 
 
 });
+
+
+Route::post('favorites', [FavoriteController::class, 'store']);
 
  // get all products
  Route::get('products', [ProductController::class, 'index']);
